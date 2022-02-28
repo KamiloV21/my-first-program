b0f5d8d7cc111b26943aaed565b707ee770f52f7
Maksim Kalin <kalinm270603@gmail.com>
2022-02-28 20:04:29 +0200

    lesson-10-oop

---
+++ ConsoleApp1/lesson-10-oop/Phone Book/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_10_oop.Phone_Book
+{
+    internal class Person
+    {
+        public string Name;
+
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        public void SetName(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ ConsoleApp1/lesson-10-oop/Phone Book/PhoneBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_10_oop.Phone_Book
+{
+    internal class PhoneBook
+    {
+        public PhoneBookRecord[] Persons;
+
+        public PhoneBook(PhoneBookRecord[] persons)
+        {
+            this.Persons = persons;
+        }
+
+        public void Print()
+        {
+            foreach (var item in this.Persons)
+            {
+                item.Print();
+            }
+        }
+    }
+}
